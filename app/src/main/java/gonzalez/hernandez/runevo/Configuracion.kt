d89707e267449111e0cfb4d3d4177c29276e3053
JesusGlezAcosta <71987633+JesusGlezAcosta@users.noreply.github.com>
2022-06-04 22:50:06 -0800

    Correcciones en diseño
    Ahora el layout de iniciar sesión pide correo y no usuario

--- app/src/main/java/gonzalez/hernandez/runevo/Configuracion.kt
+++ app/src/main/java/gonzalez/hernandez/runevo/Configuracion.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import gonzalez.hernandez.runevo.databinding.ActivityConfiguracionBinding
 import gonzalez.hernandez.runevo.ui.perfil.PerfilFragment
 
 class Configuracion : AppCompatActivity() {
@@ -16,9 +17,9 @@ class Configuracion : AppCompatActivity() {
         val imgDevolver = findViewById<ImageButton>(R.id.btnBackCreateA)
         val btnCerrarSesion = findViewById<TextView>(R.id.btn_cerrar_sesion)
 
-        imgDevolver.setOnClickListener {
-            val lanzar = Intent(this, PerfilFragment::class.java)
-            startActivity(lanzar)
+        imgDevolver.setOnClickListener{
+            val intent = Intent(this, PerfilFragment::class.java)
+            this.startActivity(intent)
         }
 
         btnCerrarSesion.setOnClickListener{
