57412b9d616515afb6ca1b69254ff1960fadda05
Rene Hernandez <rene.he24@gmail.com>
2022-06-05 13:50:21 -0700

    PerfilFragment: cambiar valor txtView en proceso

--- app/src/main/java/gonzalez/hernandez/runevo/MainActivity.kt
+++ app/src/main/java/gonzalez/hernandez/runevo/MainActivity.kt
@@ -1,12 +1,15 @@
 package gonzalez.hernandez.runevo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import gonzalez.hernandez.runevo.databinding.ActivityMainBinding
+import gonzalez.hernandez.runevo.ui.perfil.PerfilFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,15 +22,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val editText = findViewById<TextView>(R.id.txtPerfilName)
+
         AppBarConfiguration(
             setOf(
                 R.id.nav_trofeos, R.id.nav_comunidad, R.id.nav_carrera, R.id.nav_historial, R.id.nav_perfil
             )
         )
         navView.setupWithNavController(navController)
+
+        fun changeEditText(name:String){
+            editText.setText(name)
+        }
     }
 }
--- app/src/main/java/gonzalez/hernandez/runevo/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/gonzalez/hernandez/runevo/ui/perfil/PerfilFragment.kt
@@ -5,26 +5,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import gonzalez.hernandez.runevo.Configuracion
-import gonzalez.hernandez.runevo.R
-import gonzalez.hernandez.runevo.RegistrarseActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import gonzalez.hernandez.runevo.*
 import gonzalez.hernandez.runevo.databinding.FragmentPerfilBinding
 
-class PerfilFragment : Fragment() {
-    private var _binding: FragmentPerfilBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+class PerfilFragment : Fragment() {
+
+    private var _binding: FragmentPerfilBinding? = null
+    val fs = Firebase.firestore
     private val binding get() = _binding!!
 
+    companion object{
+        var perfilActivo: Perfil? = null
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         val bind = FragmentPerfilBinding.inflate(layoutInflater)
         val perfilViewModel = ViewModelProvider(this).get(PerfilViewModel::class.java)
+
+
+        fs.collection("users").whereEqualTo("email", iniciarSesion.email).get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    var perfiles = document.get("perfiles")
+                    for (perfil in perfiles as ArrayList<Any>) {
+                        perfil as HashMap<String, Any>
+                        var nombrepf = perfil.get("nombre")
+
+                        (activity as? MainActivity)?.changeEditText()
+                        Toast.makeText(activity,"Si entra " + nombrepf,Toast.LENGTH_SHORT).show();
+                        binding.txtPerfilName.text = nombrepf.toString()
+                    }
+                }
+
+                }
+
+        //////////////
 
         _binding = FragmentPerfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -40,7 +62,6 @@ class PerfilFragment : Fragment() {
         return root
 
     }
-
 
 
     override fun onDestroyView() {
